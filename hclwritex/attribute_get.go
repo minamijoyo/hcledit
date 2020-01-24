84b38532431256b8b4a9bd40411f55a3ea1a7f6f
Masayuki Morita <minamijoyo@gmail.com>
2020-01-24 15:13:46 +0900

    Get a top level attribute

---
+++ hclwritex/attribute_get.go
@@ -0,0 +1,87 @@
+package hclwritex
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/hclwrite"
+)
+
+// GetAttribute reads HCL from io.Reader, and writes a value to matched
+// attribute to io.Writer.
+// Note that a filename is used only for an error message.
+// If an error occurs, Nothing is written to the output stream.
+func GetAttribute(r io.Reader, w io.Writer, filename string, address string) error {
+	e := &Editor{
+		source: &parser{filename: filename},
+		filters: []Filter{
+			&attributeGet{address: address},
+		},
+		sink: &attributeGet{address: address},
+	}
+
+	return e.Apply(r, w)
+}
+
+// attributeGet is a filter and sink implementation for attribute.
+type attributeGet struct {
+	address string
+}
+
+// Filter reads HCL and writes only matched an attribute at a given address.
+func (f *attributeGet) Filter(inFile *hclwrite.File) (*hclwrite.File, error) {
+	// Check only top level attribute for now.
+	attrName := f.address
+	attr := inFile.Body().GetAttribute(attrName)
+
+	outFile := hclwrite.NewEmptyFile()
+	outFile.Body().SetAttributeRaw(attrName, attr.BuildTokens(nil))
+
+	return outFile, nil
+}
+
+// Sink reads HCL and writes value of attribute.
+func (f *attributeGet) Sink(inFile *hclwrite.File) ([]byte, error) {
+	attrName := f.address
+	attr := inFile.Body().GetAttribute(attrName)
+	// treat expr as a string without iterpreting its meaning.
+	out, err := getAttributeValueAsString(attr)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return []byte(out), nil
+}
+
+// getAttributeValueAsString returns a value of Attribute as string.
+// There is no way to get value as string directly,
+// so we parses tokens of Attribute and build string representation.
+func getAttributeValueAsString(attr *hclwrite.Attribute) (string, error) {
+	// find TokenEqual
+	expr := attr.Expr()
+	exprTokens := expr.BuildTokens(nil)
+	i := 0
+	for exprTokens[i].Type != hclsyntax.TokenEqual {
+		i++
+	}
+
+	if i == len(exprTokens) {
+		return "", fmt.Errorf("failed to find TokenEqual: %#v", attr)
+	}
+
+	// append tokens until find TokenComment
+	var valueTokens hclwrite.Tokens
+	for _, t := range exprTokens[(i + 1):] {
+		if t.Type == hclsyntax.TokenComment {
+			break
+		}
+		valueTokens = append(valueTokens, t)
+	}
+
+	// TokenIdent records SpaceBefore, but we should ignore it here.
+	value := strings.TrimSpace(string(valueTokens.Bytes()))
+
+	return value, nil
+}
---
+++ hclwritex/attribute_get_test.go
@@ -0,0 +1,68 @@
+package hclwritex
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestAttributeGet(t *testing.T) {
+	cases := []struct {
+		name    string
+		src     string
+		address string
+		ok      bool
+		want    string
+	}{
+		{
+			name: "simple top level attribute",
+			src: `
+a0 = v0
+a1 = v1
+`,
+			address: "a0",
+			ok:      true,
+			want:    "v0",
+		},
+		{
+			name: "attribute with comments",
+			src: `
+// attr comment
+a0 = v0 // inline comment
+a1 = v1
+`,
+			address: "a0",
+			ok:      true,
+			want:    "v0",
+		},
+		{
+			name: "duplicated attributes should be error",
+			src: `
+a0 = v0
+a0 = v1
+`,
+			address: "a0",
+			ok:      false,
+			want:    "",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			inStream := bytes.NewBufferString(tc.src)
+			outStream := new(bytes.Buffer)
+			err := GetAttribute(inStream, outStream, "test", tc.address)
+			if tc.ok && err != nil {
+				t.Fatalf("unexpected err = %s", err)
+			}
+
+			got := outStream.String()
+			if !tc.ok && err == nil {
+				t.Fatalf("expected to return an error, but no error, outStream: \n%s", got)
+			}
+
+			if got != tc.want {
+				t.Fatalf("got:\n%s\nwant:\n%s", got, tc.want)
+			}
+		})
+	}
+}
