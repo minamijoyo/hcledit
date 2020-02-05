cc7f80240961fe188558a30d90b5d0e8ca419322
Masayuki Morita <minamijoyo@gmail.com>
2020-02-05 16:57:32 +0900

    Allow getting attribute in block
    If an given address contains dots, it is ambiguous to find blocks from
    the address without a schema.
    To distinguish them in address notation requires introducing a strange new
    syntax, which is not user friendly. The address notation is not specified
    in the scope of the HCL specification, So the initial implementation has
    Terraform in mind, but we want to solve it in a schemaless way.
    We prioritize realistic usability over accuracy, we rely on some heuristics
    here to compromise.
    Given the address A.B.C, the user knows if B is a label or a nested block
    type. So if the block matched in either, we should consider it is matched.
    If you had both a label name and a nested block type, the address would
    be A.B.B.C.
    The labels take precedence over nested block. This is because if a block
    type is specified, it is assumed that the number of labels in the same block
    type does not really change and only the label name can be changed by the
    user, and we want to give the user room to avoid unintended conflicts.

--- hclwritex/attribute_get.go
+++ hclwritex/attribute_get.go
@@ -1,6 +1,7 @@
 package hclwritex
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"strings"
@@ -32,16 +33,153 @@ type attributeGet struct {
 
 // Filter reads HCL and writes only matched an attribute at a given address.
 func (f *attributeGet) Filter(inFile *hclwrite.File) (*hclwrite.File, error) {
-	// Check only top level attribute for now.
-	attrName := f.address
-	attr := inFile.Body().GetAttribute(attrName)
+	attr, err := findAttribute(inFile.Body(), f.address)
+	if err != nil {
+		return nil, err
+	}
 
 	outFile := hclwrite.NewEmptyFile()
 	if attr != nil {
-		outFile.Body().SetAttributeRaw(attrName, attr.BuildTokens(nil))
+		outFile.Body().SetAttributeRaw(f.address, attr.BuildTokens(nil))
 	}
 
 	return outFile, nil
+}
+
+// findAttribute returns first matching attribute at a given address.
+// If the address does not cantain any dots, find attribute in the body.
+// If the address contains dots, the last element is an attribute name,
+// and the rest is the address of the block.
+// The block is fetched by findLongestMatchingBlocks.
+func findAttribute(body *hclwrite.Body, address string) (*hclwrite.Attribute, error) {
+	if len(address) == 0 {
+		return nil, errors.New("failed to parse address. address is empty")
+	}
+
+	a := strings.Split(address, ".")
+	if len(a) == 1 {
+		// if the address does not cantain any dots, find attribute in the body.
+		attr := body.GetAttribute(a[0])
+		return attr, nil
+	}
+
+	// if address contains dots, the last element is an attribute name,
+	// and the rest is the address of the block.
+	attrName := a[len(a)-1]
+	blockAddr := strings.Join(a[:len(a)-1], ".")
+	blocks, err := findLongestMatchingBlocks(body, blockAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(blocks) == 0 {
+		// not found
+		return nil, nil
+	}
+
+	// if blocks are matched, check if it has a given attribute name
+	for _, b := range blocks {
+		attr := b.Body().GetAttribute(attrName)
+		if attr != nil {
+			// return first matching one.
+			return attr, nil
+		}
+	}
+
+	// not found
+	return nil, nil
+}
+
+// findLongestMatchingBlocks returns the longest matching blocks at a  given address.
+// if the address does not cantain any dots, return all matching blocks by type.
+// If the address contains dots, the first element is a block type,
+// and the rest is labels or nested block type or composite of them.
+// It is ambiguous to find blocks from the address without a schema.
+// To distinguish them in address notation requires introducing a strange new
+// syntax, which is not user friendly. The address notation is not specified
+// in the scope of the HCL specification, So the initial implementation has
+// Terraform in mind, but we want to solve it in a schemaless way.
+// We prioritize realistic usability over accuracy, we rely on some heuristics
+// here to compromise.
+// Given the address A.B.C, the user knows if B is a label or a nested block
+// type. So if the block matched in either, we should consider it is matched.
+// If you had both a label name and a nested block type, the address would be
+// A.B.B.C.
+// The labels take precedence over nested blocks. This is because if a block
+// type is specified, it is assumed that the number of labels in the same block
+// type does not really change and only the label name can be changed by the
+// user, and we want to give the user room to avoid unintended conflicts.
+func findLongestMatchingBlocks(body *hclwrite.Body, address string) ([]*hclwrite.Block, error) {
+	if len(address) == 0 {
+		return nil, errors.New("failed to parse address. address is empty")
+	}
+
+	a := strings.Split(address, ".")
+	typeName := a[0]
+	blocks := allMatchingBlocksByType(body, typeName)
+
+	if len(a) == 1 {
+		// if the address does not cantain any dots,
+		// return all matching blocks by type
+		return blocks, nil
+	}
+
+	matched := []*hclwrite.Block{}
+	// if address contains dots, the next element maybe label or nested block.
+	for _, b := range blocks {
+		labels := b.Labels()
+		// consume labels from address
+		matchedlabels := longestMatchingLabels(labels, a[1:])
+		if len(matchedlabels) < len(labels) {
+			// The labels take precedence over nested blocks.
+			// If extra labels remain, skip it.
+			continue
+		}
+		if len(matchedlabels) < (len(a)-1) || len(labels) == 0 {
+			// if the block has no labels or partially matched ones, find the nested block
+			nestedAddr := strings.Join(a[1+len(matchedlabels):], ".")
+			nested, err := findLongestMatchingBlocks(b.Body(), nestedAddr)
+			if err != nil {
+				return nil, err
+			}
+			matched = append(matched, nested...)
+			continue
+		}
+		// all labels are matched, just add it to matched list.
+		matched = append(matched, b)
+	}
+
+	return matched, nil
+}
+
+// allMatchingBlocksByType returns all matching blocks from the body that have the
+// given name or returns an empty list if there is currently no matching block.
+// This method is useful when you want to ignore label differences.
+func allMatchingBlocksByType(b *hclwrite.Body, typeName string) []*hclwrite.Block {
+	matched := []*hclwrite.Block{}
+	for _, block := range b.Blocks() {
+		if typeName == block.Type() {
+			matched = append(matched, block)
+		}
+	}
+
+	return matched
+}
+
+// longestMatchLabels returns a partial labels from the beginning to the
+// matching part and returns an empty array if nothing matches.
+func longestMatchingLabels(labels []string, prefix []string) []string {
+	matched := []string{}
+	for i := range prefix {
+		if len(labels) <= i {
+			return matched
+		}
+		if prefix[i] != labels[i] {
+			return matched
+		}
+		matched = append(matched, labels[i])
+	}
+	return matched
 }
 
 // Sink reads HCL and writes value of attribute.
--- hclwritex/attribute_get_test.go
+++ hclwritex/attribute_get_test.go
@@ -63,6 +63,138 @@ a0 = v1
 			ok:      false,
 			want:    "",
 		},
+		{
+			name: "attribute in block",
+			src: `
+b1 {
+  a1 = v1
+}
+`,
+			address: "b1.a1",
+			ok:      true,
+			want:    "v1",
+		},
+		{
+			name: "attribute in block with a label",
+			src: `
+b1 "l1" {
+  a1 = v1
+}
+`,
+			address: "b1.l1.a1",
+			ok:      true,
+			want:    "v1",
+		},
+		{
+			name: "attribute in block with multiple labels",
+			src: `
+b1 {
+  a1 = v0
+}
+b1 "l1" {
+  a1 = v1
+}
+b1 "l1" "l2" {
+  a1 = v2
+}
+b1 "l1" "l2" "l3" {
+  a1 = v3
+}
+`,
+			address: "b1.l1.l2.a1",
+			ok:      true,
+			want:    "v2",
+		},
+		{
+			name: "attribute in nested block",
+			src: `
+b1 {
+  a1 = v1
+  b2 {
+    a2 = v2
+  }
+}
+`,
+			address: "b1.b2.a2",
+			ok:      true,
+			want:    "v2",
+		},
+		{
+			name: "attribute in nested block (extra labels)",
+			src: `
+b1 "l1" {
+  a1 = v1
+  b2 {
+    a2 = v2
+  }
+}
+`,
+			address: "b1.b2.a2",
+			ok:      true,
+			want:    "",
+		},
+		{
+			name: "labels take precedence over nested blocks",
+			src: `
+b1 "b2" {
+  a1 = v1
+  b2 {
+    a1 = v2
+  }
+}
+`,
+			address: "b1.b2.a1",
+			ok:      true,
+			want:    "v1",
+		},
+		{
+			name: "attribute in multi level nested block",
+			src: `
+b1 {
+  a1 = v1
+  b2 {
+    a2 = v2
+    b3 {
+      a3 = v3
+    }
+  }
+}
+`,
+			address: "b1.b2.b3.a3",
+			ok:      true,
+			want:    "v3",
+		},
+		{
+			name: "attribute in nested block with labels",
+			src: `
+b1 {
+  a1 = v1
+  b2 "b3" {
+    a2 = v2
+    b3 {
+      a2 = v3
+    }
+  }
+}
+`,
+			address: "b1.b2.b3.a2",
+			ok:      true,
+			want:    "v2",
+		},
+		{
+			name: "attribute in duplicated blocks",
+			src: `
+b1 "l1" "l2" {
+  a1 = v1
+}
+b1 "l1" "l2" {
+  a1 = v2
+}
+`,
+			address: "b1.l1.l2.a1",
+			ok:      true,
+			want:    "v1",
+		},
 	}
 
 	for _, tc := range cases {
