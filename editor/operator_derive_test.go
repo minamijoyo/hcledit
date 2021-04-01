94a8bea72d694f02f98e4a4b249716ef0b46c9fe
Masayuki Morita <minamijoyo@gmail.com>
2021-04-01 22:56:54 +0900

    Add tests for UpdateFile, ReadFile, and DeriveFile

--- editor/operator_derive_test.go
+++ editor/operator_derive_test.go
@@ -115,3 +115,70 @@ a1 = v1
 		})
 	}
 }
+
+func TestDeriveFile(t *testing.T) {
+	cases := []struct {
+		name    string
+		src     string
+		address string
+		value   string
+		ok      bool
+		want    string
+	}{
+		{
+			name: "match",
+			src: `
+a0 = v0
+a1 = v1
+`,
+			address: "a0",
+			ok:      true,
+			want:    "v0\n",
+		},
+		{
+			name: "not found",
+			src: `
+a0 = v0
+a1 = v1
+`,
+			address: "a2",
+			ok:      true,
+			want:    "",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			path := setupTestFile(t, tc.src)
+			outStream := new(bytes.Buffer)
+			sink := NewAttributeGetSink(tc.address)
+			err := DeriveFile(path, outStream, sink)
+			if tc.ok && err != nil {
+				t.Fatalf("unexpected err = %s", err)
+			}
+
+			got := outStream.String()
+			if !tc.ok && err == nil {
+				t.Fatalf("expected to return an error, but no error, outStream: \n%s", got)
+			}
+
+			if got != tc.want {
+				t.Fatalf("got:\n%s\nwant:\n%s", got, tc.want)
+			}
+
+			input := readTestFile(t, path)
+			if input != tc.src {
+				t.Fatalf("the input file should be read-only, but changed: \n%s", input)
+			}
+		})
+	}
+}
+
+func TestDeriveFileNotFound(t *testing.T) {
+	sink := NewAttributeGetSink("foo")
+	outStream := new(bytes.Buffer)
+	err := DeriveFile("not_found", outStream, sink)
+	if err == nil {
+		t.Error("expected to return an error, but no error")
+	}
+}
---
+++ editor/test_helper.go
@@ -0,0 +1,36 @@
+package editor
+
+import (
+	"os"
+	"testing"
+)
+
+// setupTestFile creates a temporary file with given contents for testing.
+// It returns a path to the file.
+func setupTestFile(t *testing.T, contents string) string {
+	t.Helper()
+	f, err := os.CreateTemp("", "test-*.hcl")
+	if err != nil {
+		t.Fatalf("failed to create test file: %s", err)
+	}
+
+	path := f.Name()
+	t.Cleanup(func() { os.Remove(path) })
+
+	if err := os.WriteFile(path, []byte(contents), 0644); err != nil {
+		t.Fatalf("failed to write test file: %s", err)
+	}
+
+	return path
+}
+
+// readTestFile is a test helper for reading file with error handling.
+func readTestFile(t *testing.T, path string) string {
+	t.Helper()
+	b, err := os.ReadFile(path)
+	if err != nil {
+		t.Fatalf("failed to read test file: %s", err)
+	}
+
+	return string(b)
+}
